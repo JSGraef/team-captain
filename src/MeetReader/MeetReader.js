39cd2ffe2ff60ed7844955bf175734b0d1947cb5
JSGraef <jg@joshgraef.com>
2017-01-20 12:28:20 -0500

    removed Unused state variable

--- src/MeetReader/MeetReader.js
+++ src/MeetReader/MeetReader.js
@@ -12,7 +12,6 @@ class MeetReader extends Component {
 
     this.state = {
       lines: [],
-      meet: {},
       meetInfo: {},
       teams: [],
       events: []
@@ -28,7 +27,6 @@ class MeetReader extends Component {
   initialize() {
     this.setState({
       lines: [],
-      meet: {},
       meetInfo: {},
       teams: []
     });
