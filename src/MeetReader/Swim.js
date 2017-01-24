14c034f0883678fe5931375751c0c4251ad153d8
JSGraef <jg@joshgraef.com>
2017-01-24 14:42:02 -0500

    Add link to event from swimmer page

--- src/MeetReader/Swim.js
+++ src/MeetReader/Swim.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import U from './utils';
 import Splits from './Splits';
+import {Link} from 'react-router';
 
 // Shows a particular swim from a list of swims
 const Swim = (props) => {
-    const swim = props.swim;
+    const {swim} = props;
 
     const finalsTime = swim.finalsTime.substring(0,swim.finalsTime.length-1);
     let timeDiff = U.timeDiff(swim.seedTime, swim.finalsTime);
@@ -16,19 +17,20 @@ const Swim = (props) => {
 
     return (
         <tbody>
-        <tr>
-            <td className="swim-eventTitle mdl-data-table__cell--non-numeric">{`${swim.eventDist} ${U.getStrokeFromCode(swim.strokeCode)}`}</td>
-            <td>{swim.seedTime}</td>
-            <td>{swim.prelimTime}</td>
-            <td className="swim-finalTime">{finalsTime}</td>
-            <td className={timeDiffClass}>{timeDiff}</td>
-            <td>{swim.pointsScored}</td>
-        </tr>
-        <tr>
-            <td colSpan='2' className="swim-splitTitle">SPLITS:</td>
-            
-            <Splits splits={swim.splits} />
-        </tr>
+            <tr>
+                <td className="swim-eventTitle mdl-data-table__cell--non-numeric">
+                    <Link to={`/meetreader/events/${swim.eventNum}`}>{`${swim.eventDist} ${U.getStrokeFromCode(swim.strokeCode)}`}</Link>
+                </td>
+                <td>{swim.seedTime}</td>
+                <td>{swim.prelimTime}</td>
+                <td className="swim-finalTime">{finalsTime}</td>
+                <td className={timeDiffClass}>{timeDiff}</td>
+                <td>{swim.pointsScored}</td>
+            </tr>
+             <tr>
+                <td colSpan='2' className="swim-splitTitle">SPLITS:</td>
+                <Splits splits={swim.splits} />
+            </tr>
         </tbody>
     )}
 
