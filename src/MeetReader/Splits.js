b82327793a10616014da3af16d7aec53173bce9a
JSGraef <jg@joshgraef.com>
2017-01-24 14:41:42 -0500

    Add link to event from swimmer page

--- src/MeetReader/Splits.js
+++ src/MeetReader/Splits.js
@@ -6,39 +6,29 @@ import U from './utils';
 const Splits = (props) => {
     const allSplits = props.splits;
 
-    let splits = [];
-    //console.log(allSplits.length);
-    if(allSplits.length === 1) {
-        splits.push( allSplits[0].splitTime1 );
-        splits.push( allSplits[0].splitTime2 );
-        splits.push( allSplits[0].splitTime3 );
-        splits.push( allSplits[0].splitTime4 );
-        splits.push( allSplits[0].splitTime5 );
-        splits.push( allSplits[0].splitTime6 );
-        splits.push( allSplits[0].splitTime7 );
-        splits.push( allSplits[0].splitTime8 );
-        splits.push( allSplits[0].splitTime9 );
-        splits.push( allSplits[0].splitTime10 );
-    }
-    else if(allSplits.length === 2) {
-        splits.push( allSplits[1].splitTime1 );
-        splits.push( allSplits[1].splitTime2 );
-        splits.push( allSplits[1].splitTime3 );
-        splits.push( allSplits[1].splitTime4 );
-        splits.push( allSplits[1].splitTime5 );
-        splits.push( allSplits[1].splitTime6 );
-        splits.push( allSplits[1].splitTime7 );
-        splits.push( allSplits[1].splitTime8 );
-        splits.push( allSplits[1].splitTime9 );
-        splits.push( allSplits[1].splitTime10 );
-    }
-
-
     return (
-        <td colSpan="4" className="swim-splitTd">
-            { splits.map( split => {
-                return <span key={U.guid()} className='swim-split'>{split}</span>
-            })}
+        <td>
+            { allSplits.map( s => {
+                let splits = [];
+                splits.push( s.splitTime1 );
+                splits.push( s.splitTime2 );
+                splits.push( s.splitTime3 );
+                splits.push( s.splitTime4 );
+                splits.push( s.splitTime5 );
+                splits.push( s.splitTime6 );
+                splits.push( s.splitTime7 );
+                splits.push( s.splitTime8 );
+                splits.push( s.splitTime9 );
+                splits.push( s.splitTime10 );
+
+                return (
+                        <td key={U.guid()}  colSpan="4" className="swim-splitTd">
+                            { splits.map( split => {
+                                return <span key={U.guid()} className='swim-split'>{split}</span>
+                            })}
+                        </td>
+                )})}    
+                <br/>
         </td>
             
     )}
