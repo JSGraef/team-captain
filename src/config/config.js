931707e2b032ea31cf8bb1337b603d83dc024f6c
JSGraef <jg@joshgraef.com>
2018-08-20 10:46:17 -0400

    no need to add that

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-   apiKey: "AIzaSyA9Bp3nt7764_Rco-fHAwRsld6DyWhS1B4",
-   authDomain: "teamcaptain-b3334.firebaseapp.com",
-   databaseURL: "https://teamcaptain-b3334.firebaseio.com",
-   storageBucket: "teamcaptain-b3334.appspot.com",
-   messagingSenderId: "602452524568"
- };
